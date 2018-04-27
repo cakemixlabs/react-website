ec17268728a32a8e71f0b89df4e23ac81e6137e8
Rhonda Glennon <rhondaglennon@gmail.com>
2018-04-27 18:50:31 -0400

    remove extra files and get hello world page running

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+      return (
+        <div>
+            Coming soon!
+        </div>
+      );
+    }
+}
+
+
+export default Navbar;
