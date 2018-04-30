6118263b090dd98c647b6a7e76c6254f0aafedeb
Rhonda Glennon <rhondaglennon@gmail.com>
2018-04-30 15:00:17 -0400

    add navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 class Navbar extends Component {
     render() {
-      return (
-        <div>
-            Coming soon!
-        </div>
-      );
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">Cakemix Labs</a>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-item nav-link" href="#">Features</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        );
     }
 }
 
