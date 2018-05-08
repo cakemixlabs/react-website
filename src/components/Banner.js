3c2e3cd0725237873c0be5f028418c37e42475c3
Rhonda Glennon <rhondaglennon@gmail.com>
2018-05-07 20:58:46 -0700

    update page text and style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 class Banner extends Component {
     render() {
         return (
-            <div className="jumbotron">
-                <h1 className="display-4">AI for location</h1>
-                <p className="lead">Cakemix Labs</p>
+            <div className="jumbotron text-center">
+                <h1 className="display-4">Artificial intelligence for location</h1>
+                <p className="lead">Solving complex spatial problems</p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
             </div>
         );
     }
